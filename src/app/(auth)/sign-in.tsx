4ff3b1c9f7d8d3f3afadaf249b69f9fe040ec91a
Drew <chukwukaenudeme@gmail.com>
2026-03-16 10:56:21 +0100

    feat: enhance authentication layout and styling, add decorative elements to sign-in screen

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,7 +1,5 @@
-import { AuthView } from "@clerk/expo/native";
 import { useAuth } from "@clerk/expo";
 import { useRouter } from "expo-router";
-import { useEffect } from "react";
 import useSocialAuth from "@/hooks/useSocialAuth";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -10,8 +8,10 @@ export default function SignInScreen() {
   const { handleSocialAuth, loadingStrategy } = useSocialAuth();
 
   return (
-    <SafeAreaView>
-      <Text className="flex-1 text-red-500 bg-primary ">Hey everyone</Text>
+    <SafeAreaView className="flex-1 bg-primary dark:bg-secondary">
+      {/* decorative elements */}
+      <View className="absolute -left-16 top-12 h-56 w-56 rounded-full bg-primary/80 dark:bg-background/40" />
+      <View className="absolute right-[-74px] top-40 h-72 w-72 rounded-full bg-primary/70 dark:bg-background/35" />
     </SafeAreaView>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,11 @@ import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { Stack } from "expo-router";
 import "../../global.css";
-import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { useColorScheme } from "react-native";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -10,15 +14,14 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
+
 export default function RootLayout() {
-  const colorSheme = useColorScheme();
+  const colorScheme = useColorScheme();
   return (
-    <>
-      <ClerkProvider publishableKey={publishableKey}>
-        <ThemeProvider value={colorSheme === "dark" ? DarkTheme : DarkTheme}>
-          <Stack screenOptions={{ headerShown: false }} />
-        </ThemeProvider>
-      </ClerkProvider>
-    </>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
